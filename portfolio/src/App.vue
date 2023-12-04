7bae2fb0375c678d52b9c080a156aefcbe16777f
Alex "Splitz" Davis <mead0wz0edavis@gmail.com>
2023-12-04 16:03:43 +0000

    Added views for pages of the site
    Added the basic webpages for the website as well as adding a component for showing the portfolio examples

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/gamedev">Game Dev</router-link> |
+    <router-link to="/webdev">Web Dev</router-link>
   </nav>
   <router-view/>
 </template>
