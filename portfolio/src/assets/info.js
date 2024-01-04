782967673e4265eed6a5fe8f3e6303007865b216
Alex "Splitz" Davis <mead0wz0edavis@gmail.com>
2024-01-04 14:45:32 +0000

    Created Information for project cards
    Started the basis on the information that'll be added to the project cards within the portfolio section

---
+++ portfolio/src/assets/info.js
@@ -0,0 +1,115 @@
+let info = {
+    gameDevelopment: 
+    [
+        {
+            name: "Dead Methany...",
+            date: "12/2020",
+            shortDescription: "",
+            longDescription: "",
+            skills: [
+                "Bitsy",
+                "JavaScript"
+            ],
+            link: ""
+        },
+        {
+            name: "Uppercut!",
+            date: "5/2020",
+            shortDescription: "",
+            longDescription: "",
+            skills: [
+                "Unity",
+                "C#"
+            ],
+            link: ""
+        },
+        {
+            name: "Pose of Faith",
+            date: "3/2020",
+            shortDescription: "",
+            longDescription: "",
+            skills: [
+                "Unity",
+                "C#"
+            ],
+            link: ""
+        },
+        {
+            name: "Block Assault",
+            date: "",
+            shortDescription: "",
+            longDescription: "",
+            skills: [
+                "Unity",
+                "C#"
+            ],
+            link: ""
+        }
+    ],
+    webDevelopment: 
+    [
+        {
+            name: "Portfolio Website",
+            date: "1/2024",
+            shortDescription: "Worked on this Portfolio Website",
+            longDescription: "",
+            skills: [
+                "VueJS",
+                "Tailwind",
+                "NodeJS"
+            ],
+            link: ""
+        },
+        {
+            name: "GGST Move Viewer",
+            date: "Ongoing",
+            shortDescription: "",
+            longDescription: "",
+            skills: [
+                "VueJS",
+                "NodeJS",
+                "CSS",
+                "JSON"
+            ],
+            link: ""
+        },
+        {
+            name: "GGST Stats Website",
+            date: "2/2023",
+            shortDescription: "Worked on UI elements for the GGST Stats website",
+            longDescription: "Worked on the basic UI elements of the GGST Stats to display the data.",
+            skills: [
+                "HTML",
+                "CSS"
+            ],
+            link: ""
+        },
+        {
+            name: "The Odin Project",
+            date: "2023 - Ongoing",
+            shortDescription: "",
+            longDescription: "",
+            skills: [
+                "HTML",
+                "CSS",
+                "NodeJS",
+                "JavaScript"
+            ],
+            link: ""
+        },
+        {
+            name: "Online Rock Paper Scissors",
+            date: "3/2020",
+            shortDescription: "",
+            longDescription: "",
+            skills: [
+                "NodeJS",
+                "WebSocket",
+                "CSS",
+                "HTML"
+            ],
+            link: ""
+        }
+
+    ]
+}
