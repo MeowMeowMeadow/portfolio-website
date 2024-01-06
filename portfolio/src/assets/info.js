b78d28217df06734f0fb2def5063b563b993901e
Alex "Splitz" Davis <mead0wz0edavis@gmail.com>
2024-01-06 14:55:02 +0000

    Finished info script
    Finished the js file filled with information to place within the Portfolio sections

--- portfolio/src/assets/info.js
+++ portfolio/src/assets/info.js
@@ -4,46 +4,47 @@ let info = {
         {
             name: "Dead Methany...",
             date: "12/2020",
-            shortDescription: "",
-            longDescription: "",
+            shortDescription: "A Bitsy Game about a Godspeed You! Black Emperor Album.",
+            longDescription: "A Bitsy Game that acts as a Stream of Conciousness to discuss F# A# Inifity by Godspeed You! Black Emperor. It also uses JavaScript hacks to emphacise the experience as well.",
             skills: [
                 "Bitsy",
-                "JavaScript"
+                "JavaScript",
+                "Game Writing"
             ],
-            link: ""
+            link: "https://meadowfaust.itch.io/dead-metheny"
         },
         {
             name: "Uppercut!",
             date: "5/2020",
-            shortDescription: "",
-            longDescription: "",
+            shortDescription: "A 2D fighter with a focus on ease of use.",
+            longDescription: "A 2D fighting game made for my Final Year Project at University with a focus on ease of use. Inspired by games such as Rising Thunder and other simplistic fighting games.",
             skills: [
                 "Unity",
                 "C#"
             ],
-            link: ""
+            link: "https://splitzy.itch.io/uppercut"
         },
         {
             name: "Pose of Faith",
             date: "3/2020",
-            shortDescription: "",
-            longDescription: "",
+            shortDescription: "A endless scroller about posing for a Game Jam.",
+            longDescription: "A endless scroller about posing for the Jamfuser 2020 made with Jack Fletcher under the Cat and Mouse Studios.",
             skills: [
                 "Unity",
                 "C#"
             ],
-            link: ""
+            link: "https://catandmousestudios.itch.io/pose-of-faith"
         },
         {
             name: "Block Assault",
-            date: "",
-            shortDescription: "",
-            longDescription: "",
+            date: "1/2019",
+            shortDescription: "A racing game where you avoid obstacles and try to beat the timer.",
+            longDescription: "A racing game where you try to avoid obstacles, stay on the course and beat the timer. Made during the 2nd year of University and was improved on during the 3rd year as well.",
             skills: [
                 "Unity",
                 "C#"
             ],
-            link: ""
+            link: "https://splitzy.itch.io/block-assault"
         }
     ],
     webDevelopment: 
@@ -52,63 +53,63 @@ let info = {
             name: "Portfolio Website",
             date: "1/2024",
             shortDescription: "Worked on this Portfolio Website",
-            longDescription: "",
+            longDescription: "This portfolio website that you're currently seeing right now was made by myself using the following technologies below. This is to show off my UI/UX skills and showcase my progress with Web Development as a whole.",
             skills: [
                 "VueJS",
                 "Tailwind",
                 "NodeJS"
             ],
-            link: ""
+            link: "https://github.com/MeowMeowMeadow/portfolio-website"
         },
         {
             name: "GGST Move Viewer",
-            date: "Ongoing",
-            shortDescription: "",
-            longDescription: "",
+            date: "2023 - Ongoing",
+            shortDescription: "A Move Viewer for Guilty Gear Strive",
+            longDescription: "A Website App that shows the moves and hitboxes of each character in Guilty Gear Strive. All images are sourced from Dustloop Wiki and is still in progress.",
             skills: [
                 "VueJS",
                 "NodeJS",
                 "CSS",
                 "JSON"
             ],
-            link: ""
+            link: "https://github.com/MeowMeowMeadow/strive-move-viewer"
         },
         {
             name: "GGST Stats Website",
             date: "2/2023",
             shortDescription: "Worked on UI elements for the GGST Stats website",
-            longDescription: "Worked on the basic UI elements of the GGST Stats to display the data.",
+            longDescription: "Worked on the basic UI elements of the GGST Stats to display the data. Created by Maria Artemis with a GNU AGPL License. Note: Website & main git repo is currently down.",
             skills: [
                 "HTML",
                 "CSS"
             ],
-            link: ""
+            link: "https://github.com/MeowMeowMeadow/ggst-stats-website"
         },
         {
             name: "The Odin Project",
             date: "2023 - Ongoing",
-            shortDescription: "",
-            longDescription: "",
+            shortDescription: "Currently working on The Odin Project's self-taught Web Development course.",
+            longDescription: "Currently working on The Odin Project's self-taught Web Development course to freshen up my knowledge of JavaScript, NodeJS and other Web Development techniques. The link will send you to the Odin Dashboard that I worked on.",
             skills: [
                 "HTML",
                 "CSS",
                 "NodeJS",
                 "JavaScript"
             ],
-            link: ""
+            link: "https://meowmeowmeadow.github.io/odin-dashboard/"
         },
         {
             name: "Online Rock Paper Scissors",
-            date: "3/2020",
-            shortDescription: "",
-            longDescription: "",
+            date: "1/2020",
+            shortDescription: "A Rock Paper Scissors app made for a University Coursework",
+            longDescription: "A Rock Paper Scissors app that uses technologies such as WebSockets to connect two clients together to play a competitive match of Rock Paper Scissors.",
             skills: [
                 "NodeJS",
                 "WebSocket",
                 "CSS",
                 "HTML"
             ],
-            link: ""
+            link: "https://github.com/Splitzy/SOFT355-Coursework"
         }
 
     ]
